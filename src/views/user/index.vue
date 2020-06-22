b43f6fb37e4875ea6cb5f7c4cabdb96ea1a86b2f
余晓波 <edk24@qq.com>
2020-06-22 15:39:44 +0800

    余晓波 update

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="app-container">
 
-<el-row>
+    <el-row>
       <el-input v-model="keyword" placeholder="请输入关键词" style="width:260px" />
       <el-button type="primary" @click="fetchData(true)">搜索</el-button>
 
     </el-row>
 
-<p></p>
-<p></p>
-<!-- <el-tabs v-model="status" @tab-click="fetchData(true)">
+    <p />
+    <p />
+    <!-- <el-tabs v-model="status" @tab-click="fetchData(true)">
     <el-tab-pane label="家长" name="all"></el-tab-pane>
     <el-tab-pane label="学生" name="waitpay"></el-tab-pane>
     <el-tab-pane label="学校" name="waitdelivery"></el-tab-pane>
@@ -19,10 +19,10 @@
     <el-tab-pane label="未知" name="recived"></el-tab-pane>
   </el-tabs> -->
 
-    <el-table :data="userList" stripe style="width: 100%" >
+    <el-table :data="userList" stripe style="width: 100%">
       <el-table-column label="头像" fixed>
         <template slot-scope="scope">
-          <el-avatar shape="square" :size="50" :src="scope.row.avatar"></el-avatar>
+          <el-avatar shape="square" :size="50" :src="scope.row.avatar" />
         </template>
       </el-table-column>
       <el-table-column label="用户类型">
@@ -44,8 +44,8 @@
       <el-table-column prop="jifen" label="积分" />
       <el-table-column label="实名状态">
         <template slot-scope="scope">
-        <span v-if="scope.row.audit!==3">通过</span>
-        <span v-else>-</span>
+          <span v-if="scope.row.audit!==3">通过</span>
+          <span v-else>-</span>
         </template>
       </el-table-column>
       <el-table-column label="性别">
@@ -57,18 +57,18 @@
       </el-table-column>
       <el-table-column label="地址">
         <template slot-scope="scope">
-          {{scope.row.province}} {{scope.row.city}} {{scope.row.area}}
-          <p>{{scope.row.address}}</p>
+          {{ scope.row.province }} {{ scope.row.city }} {{ scope.row.area }}
+          <p>{{ scope.row.address }}</p>
         </template>
       </el-table-column>
       <el-table-column prop="create_time" label="注册时间" />
       <el-table-column label="管理" width="200">
         <el-button-group>
-        <el-button size="mini" type="primary">查看孩子</el-button>
-        <el-button size="mini" type="primary">学校信息</el-button>
-        <el-button size="mini" type="primary">公司信息</el-button>
-        <el-button size="mini" type="primary">机构信息</el-button>
-        <el-button size="mini" type="danger">删除</el-button>
+          <el-button size="mini" type="primary">查看孩子</el-button>
+          <el-button size="mini" type="primary">学校信息</el-button>
+          <el-button size="mini" type="primary">公司信息</el-button>
+          <el-button size="mini" type="primary">机构信息</el-button>
+          <el-button size="mini" type="danger">删除</el-button>
         </el-button-group>
       </el-table-column>
     </el-table>
@@ -81,33 +81,33 @@
 </template>
 
 <script>
-import {user_list} from '@/api/user';
-export default{
-  data(){
+import { user_list } from '@/api/user'
+export default {
+  data() {
     return {
-      userList:[],
-      count:0,
-      page:1,
-      limit:25,
-      keyword:'',
-      type:'',
+      userList: [],
+      count: 0,
+      page: 1,
+      limit: 25,
+      keyword: '',
+      type: ''
     }
   },
-  created(){
+  created() {
     this.fetchData()
   },
-  methods:{
+  methods: {
 
     // 抓取数据
-    fetchData(clear=false) {
+    fetchData(clear = false) {
       if (clear) {
-        this.userList = [];
-        this.page=1
+        this.userList = []
+        this.page = 1
       }
 
-      user_list(this.page, this.limit, this.keyword).then(({code, msg, data,count})=>{
-        if (code===0) {
-          data.forEach(user=>{
+      user_list(this.page, this.limit, this.keyword).then(({ code, msg, data, count }) => {
+        if (code === 0) {
+          data.forEach(user => {
             this.userList.push(user)
           })
           this.page++
@@ -116,7 +116,9 @@ export default{
           this.$message.warning(msg)
         }
       })
-    }
+    },
+  
+
   }
 }
 </script>
