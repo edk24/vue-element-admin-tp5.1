839bd5c38cc5eaa09cc12bc6cdc1a34ad9e1b9a9
余晓波 <edk24@qq.com>
2020-06-28 18:46:07 +0800

    余晓波 update

--- src/api/menu.js
+++ src/api/menu.js
@@ -8,3 +8,42 @@ export function get_menu_tree() {
     method:'get'
   })
 }
+
+// 查询菜单列表
+export function get_menu_list() {
+  return request({
+    url: '/v1/menu'
+  })
+}
+
+// 创建菜单
+export function create_menu(data) {
+  return request({
+    url: '/v1/menu',
+    method: 'post',
+    headers: {
+      'Content-Type':'application/json'
+    },
+    data
+  })
+}
+
+// 修改菜单
+export function update_menu(id,data) {
+  return request({
+    url: '/v1/menu/'+id,
+    method: 'put',
+    headers: {
+      'Content-Type':'application/json'
+    },
+    data
+  })
+}
+
+// 删除菜单
+export function del_menu(id) {
+  return request({
+    url:'/v1/menu/'+id,
+    method:'delete'
+  })
+}
